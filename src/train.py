4a6d0bfb37b4d7df508e6cc1209e1c7500419fee
varsha-raveendran <avr.varsha@gmail.com>
2024-05-31 09:23:08 +0200

    Removed folders

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 from model_pytorch import build_transformer
-from ..dataset import BillingualDataset, casual_mask
-from ..config_file import get_config, get_weights_file_path
+from dataset import BillingualDataset, casual_mask
+from config_file import get_config, get_weights_file_path
 
 import torchtext.datasets as datasets
 import torch
